60d7359ec57a592de2e7895b8e6e09000e462515
mahantheshhv <mantheshv@gmail.com>
2015-12-11 22:38:41 +0530

    Update bootup.py

--- scripts/bootup.py
+++ scripts/bootup.py
@@ -1,10 +1,10 @@
 import tweepy
 from datetime import datetime
 
-CONSUMER_KEY ="wMVDcLXR4kTXGUqJX0e0sUuMv"
-CONSUMER_SECRET = "tYGWxxmO8LjiwdrlqamB8N1Ia53mLQbUioQbrPgjqVas8TDNUu"   
-ACCESS_KEY = "4384993475-4nl6kNzqeM1KzbrpWUW0sENOEH1l8AJgaj8Knia"    
-ACCESS_SECRET = "E9AgwH7WE1qq26VoFtrUrLi7BLCpVNHRRZZOYCIjxfXRe"
+CONSUMER_KEY =""
+CONSUMER_SECRET = ""   
+ACCESS_KEY = ""    
+ACCESS_SECRET = ""
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)

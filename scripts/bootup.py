122f991869f4bd5c7e373210bb063c5089e3e361
mahantheshhv <mantheshv@gmail.com>
2015-12-12 20:16:05 +0530

    Update bootup.py

--- scripts/bootup.py
+++ scripts/bootup.py
@@ -1,7 +1,11 @@
 import tweepy
 from datetime import datetime
+import sys
+
+sys.path.append('/home/pi')
 
 from keys import *
+
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
